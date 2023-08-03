d73282df973b2eb24d8d4e222bd77e7340242609
Fgonzalez409 <fergonz409@gmail.com>
2023-08-02 23:15:44 -0500

    added like button to app

--- src/components/beer.jsx
+++ src/components/beer.jsx
@@ -1,10 +1,32 @@
+import React, { useState,useEffect } from "react";
+import Heart from "react-animated-heart";
+
 const Beer = (props) => {
+    const[isLiked,setIsLiked] = useState(false)
+
+    // acts as a componentDidMount lifecycle
+    useEffect(() => {
+
+    }, [])
+
+    //acts as a componentDidUpdate
+    useEffect(() => {
+
+    }, [])
+
     return (
     <li className="item" key={props.id}>
         <h2>{props.name}</h2>
         <img className="item__image" 
         src={props.image} 
         alt={props.name} />
+        <div>
+            <button  onClick={() => setIsLiked(!isLiked)}>
+                {isLiked ? "Liked" : "Hated"}
+                {/* <Heart className="heart"isClick={isClick} onClick={() => setClick(!isClick)}/>     */}
+            </button>
+        </div>
+        
     </li>
     )
 }
